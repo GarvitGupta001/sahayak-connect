25f4133ff4e6adf2ec651d272e2c01654b27ceca
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-08-17 20:02:05 +0530

    navbar and app routing

---
+++ next-app/src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { BottomNavigation, BottomNavigationAction, Icon } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import PersonIcon from "@mui/icons-material/Person";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+    const pathname = usePathname();
+
+    const [value, setValue] = useState(pathname.split("/")[2]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <BottomNavigation
+            sx={{
+                width: "100%",
+                position: "fixed",
+                bottom: 0,
+                left: 0,
+                boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+            }}
+            value={value}
+            onChange={handleChange}
+        >   
+            <BottomNavigationAction
+                component={Link}
+                href="/app/home"
+                label="Home"
+                value="home"
+                icon={<HomeIcon />}
+            />
+            <BottomNavigationAction
+                component={Link}
+                href="/app/search"
+                label="Search"
+                value="search"
+                icon={<SearchIcon />}
+            />
+            <BottomNavigationAction
+                component={Link}
+                href="/app/saved"
+                label="Saved"
+                value="saved"
+                icon={<BookmarkIcon />}
+            />
+            <BottomNavigationAction
+                component={Link}
+                href="/app/profile"
+                label="Profile"
+                value="profile"
+                icon={<PersonIcon />}
+            />
+        </BottomNavigation>
+    );
+};
+
+export default Navbar;
