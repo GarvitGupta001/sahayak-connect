343a8aeac26cfa75680b8aeb9061423aaefbb4b2
Satyamzz <satyamsingh18177@gmail.com>
2025-08-30 14:31:19 +0530

    gunicorn addition

--- ML-model/src/getting_schemes.py
+++ ML-model/src/getting_schemes.py
@@ -112,12 +112,4 @@ def submit_data():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=7860)
-
-import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+    app.run(debug=True)
