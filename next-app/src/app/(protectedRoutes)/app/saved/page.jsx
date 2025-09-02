6ced5397fc090ee06bf1e6ed03d27328c34647c2
sinhashubhika <dr.asinha@ce.du.ac.in>
2025-09-02 18:30:29 +0530

    something

--- next-app/src/app/(protectedRoutes)/app/saved/page.jsx
+++ next-app/src/app/(protectedRoutes)/app/saved/page.jsx
@@ -9,6 +9,8 @@ const Saved = () => {
       <h2 className='font-bold text-[#100027] text-xl mb-4'>Fill out FORM6 for applying for Voter ID</h2>
       <a 
       href="http://localhost:5000" 
+      target="_blank" 
+      rel="noopener noreferrer"
       className="bg-[#034f57] text-white px-4 py-2 rounded-lg font-semibold hover:bg-[#46868e] transition-colors mb-4 inline-block">
       Fill Form with AI Assistant
       </a>
